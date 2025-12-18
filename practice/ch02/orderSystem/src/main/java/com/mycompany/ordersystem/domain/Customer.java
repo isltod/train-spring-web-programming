be7bfb163bf81b49296636a9470e84a7d1540e33
E. K. Lee <wolf@teoal.net>
2025-12-19 03:30:59 +0900

    02장 practice 고객 처리까지 실습

---
+++ practice/ch02/orderSystem/src/main/java/com/mycompany/ordersystem/domain/Customer.java
@@ -0,0 +1,51 @@
+package com.mycompany.ordersystem.domain;
+
+// 도메인 객체는 스프링 빈으로 만들지 않는 모양인데...왜 그렇지? 이것도 자주 바뀔텐데?
+public class Customer {
+    private long id;
+    private String name;
+    private String address;
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
